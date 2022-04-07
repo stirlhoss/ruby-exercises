bfc02ef901ed82f125a8a729279e16ef85d4be94
Stirling Hostetter <stirhoss@icloud.com>
2022-04-07 16:08:09 -0500

    Complete Vampire

---
+++ mythical-creatures/lib/vampire.rb
@@ -0,0 +1,15 @@
+# lib/vampire.rb
+
+class Vampire
+  attr_reader :name, :pet, :thirsty
+
+  def initialize(name, pet = "bat", thirsty = true)
+    @name = name
+    @pet = pet
+    @thirsty = thirsty
+  end
+
+  def drink
+    @thirsty = false
+  end
+end
